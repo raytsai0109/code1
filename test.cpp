6848918f0f7b7f7fc7e1ecdc21f09df26462f6e7
raytsai0109 <ray.tsai.tp@gmail.com>
2025-10-13 21:15:56 +0800

    10/13 uncompress

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int main() {
-    string a="012";
-    int b=3;
-    a+=to_string(b);
-    cout<<a;
+int main(){
+    string s="'";
+    int i=isalpha(s[0]);
+    int b=isdigit(s[0]);
+    cout<<i<<endl;
+    cout<<b<<endl;
 }
--- uncompress.cpp
+++ uncompress.cpp
@@ -2,10 +2,46 @@
 using namespace std;
 
 int main(){
-    string a;
-    vector<string>l;
-    cin>>a;
-
-
-    return 0;
+    string s;
+    vector<string>smap;
+    while(getline(cin,s)){
+        if(s=="0"){
+            break;
+        }
+        s+='\n';
+        int nnum=0;
+        string nows="";
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                nnum*=10;
+                nnum+=s[i]-'0';
+            }else if(isalpha(s[i])){
+                nows+=s[i];
+            }else{
+                if(nnum!=0){
+                    int ss=smap.size();
+                    string tword=smap[ss-nnum];
+                    cout<<tword;
+                    auto finds=find(smap.begin(),smap.end(),tword);
+                    smap.erase(finds);
+                    smap.push_back(tword);
+                    cout<<s[i];
+                }else if(nows!=""){
+                    cout<<nows;
+                    auto finds=find(smap.begin(),smap.end(),nows);
+                    if(finds==smap.end()){
+                        smap.push_back(nows);
+                    }else{
+                        smap.erase(finds);
+                        smap.push_back(nows);
+                    }
+                    cout<<s[i];
+                }else{
+                    cout<<s[i];
+                }
+                nnum=0;
+                nows="";
+            }
+        }
+    }
 }
